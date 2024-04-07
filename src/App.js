491ec30bb12ca92cf6301dcaa14189ee76df1059
Shailendra Chauhan <pro.shailendra@gmail.com>
2024-04-07 14:44:09 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import Jsx, { Greet } from "./containers/Jsx";
 import DataBinding from './containers/Databinding'
 import Users from './containers/Users'
+import { Routes, NavLink, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div>
-      {/*
-            <Jsx />   
-            {
-                Greet()
-            }
-      */}
-      <DataBinding/>   
-      <Users />
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">Jsx</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/binding">DataBinding</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/users">Users</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Jsx />}></Route>
+          <Route path="/binding" element={<DataBinding />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+        </Routes>
+      </div>
     </div>
   )
 }
