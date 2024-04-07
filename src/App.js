ab4625f5321fcb31d721eb826d813a934bbb1a17
Shailendra Chauhan <pro.shailendra@gmail.com>
2024-04-07 08:27:09 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Jsx, { Greet } from "./containers/Jsx";
+import DataBinding from './containers/Databinding'
+import Users from './containers/Users'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/*
+            <Jsx />   
+            {
+                Greet()
+            }
+      */}
+      <DataBinding/>   
+      <Users />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/containers/Jsx.js
@@ -0,0 +1,69 @@
+let name = 'Mohan';
+function CheckEvenOrOdd(num) {
+  if (num % 2 === 0) {
+    return <div>Even</div>
+  }
+  else {
+    return <div>Odd</div>
+  }
+}
+function CheckAlphabet(char) {
+  switch (char) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      return <div>Vowel</div>
+    default:
+      return <div>Not Vowel</div>
+  }
+}
+let num = 4;
+const users = [
+  { name: 'Mohan', age: 25 },
+  { name: 'Ram', age: 24 },
+  { name: 'Rohit', age: 21 }
+]
+function Jsx() {
+  return (
+    <div>
+      <h2>Hello {name}</h2>
+      <div>
+        <h3>If Else Statement</h3>
+        {
+          CheckEvenOrOdd(5)
+        }
+        {
+          num % 2 === 0
+            ? <div>Even Number</div>
+            : <div>Odd Number</div>
+        }
+        <h3>If Statement</h3>
+        {
+          num % 2 === 0 && <div>Even Number</div>
+        }
+        {
+          num % 2 > 0 && <div>Odd Number</div>
+        }
+      </div>
+      <h3>Loop</h3>
+      {
+        users.map((item, index) => {
+          return <div key={index}>
+            {item.name} - {item.age}</div>
+        })
+      }
+      <h3>Switch</h3>
+      {
+        CheckAlphabet('z')
+      }
+    </div>
+  );
+}
+
+export default Jsx;
+
+export function Greet() {
+  return <h1>Hello World</h1>
+}
